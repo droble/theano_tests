b1aab6fd7540aa989aea2e05e52a335bfe715c60
Doug Roble <doug@judyjetson.d2.com>
2017-03-08 18:09:41 -0800

    Full code for logisticRegression. There's definitely an error in this puppy.

--- theano_grad.py
+++ theano_grad.py
@@ -5,10 +5,17 @@ Created on Mon Mar 06 22:25:31 2017
 @author: Admin
 """
 
-import numpy
+import numpy as np
 import theano
 import theano.tensor as T
 from theano import pp
+
+# Make some data for the regression. Hours studied vs whether the 
+# student passed or failed. (From the Wikipedia Logistic Regression page.)
+
+hours = np.array ( [[0.50,0.75,1.00,1.25,1.50,1.75,1.75,2.00,2.25,2.50,
+                     2.75,3.00,3.25,3.50,4.00,4.25,4.50,4.75,5.00,5.50]] ).T
+passfail = np.array ( [0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,1,1,1,1,1] )
 
 def doScalarDerivative() : 
     x = T.dscalar( 'x' )
@@ -30,10 +37,81 @@ def doSimpleLogistic() :
 
     # end doSimpleLogistic
 
-def logisticRegression() :
+def logisticRegression ( input_values, target_class, training_steps = 10 ) :
+    """Logisitic Regression using Theano
     
+    input_values -- N x features matrix. Each row is a location in 
+    feature-dimensional space. 
     
+    target_class -- N element vector, classifying each location 
+    as either 1 or 0. 
+    """
+    
+    N     = input_values.shape[0]
+    feats = input_values.shape[1]
+    
+    if target_class.shape[0] != N : 
+        raise ValueError ( "Number of rows in input_values must match number "
+        "of rows in target_class." )
+        
+    # Declare the symbolic variables. 
+        
     X = T.dmatrix( "X" ) 
-    Y = T.dmatrix( "Y" )
+    Y = T.dvector( "Y" )
+    
+    # Initialize the weight vector and the bias. 
+    
+    w = theano.shared( np.random.randn( feats ), name="w" )
+    b = theano.shared( 0., name="b" )
+    
+    print ( "Initial model: " )
+    print ( w.get_value() )
+    print ( b.get_value() )
+    print ( "Input data" )
+    print ( "input_values: ", input_values.shape )
+    print ( input_values )
+    print ( "target_class: ", target_class.shape )
+    print ( target_class )
+    
+    # Construct the Theano expression graph. 
+    
+    t = T.dot( X, w ) - b
+    p_1 = 1 / ( 1 + T.exp( -t ) )
+    
+    prediction = p_1 > 0.5 
+    
+    xent = -Y * T.log( p_1 ) - ( 1 - Y ) * T.log( 1 - p_1 )
+    cost = xent.mean() + 0.01 * ( w ** 2 ).sum() 
+    
+    dw, db = T.grad ( cost, [w, b] )
+    
+    # Compile it. 
+    
+    train = theano.function( inputs = [X, Y], 
+                             outputs = [prediction, xent], 
+                             updates = ( ( w, w - 0.1 * dw ), 
+                                         ( b, b - 0.1 * db ) ) )
+                                         
+    predict = theano.function( inputs = [X], outputs = prediction )
+    
+    # Try an initial prediction. 
+    
+    print "Initial prediction: "
+    print predict ( input_values )
+    
+    # Train it! 
+    
+    for i in xrange( training_steps ) : 
+        pred, err = train ( ( input_values, target_class ) )
+        
+    print( "Final model:" )
+    print( w.get_value() )
+    print( b.get_value() )
+    print( "target values for D:" )
+    print( input_values )
+    print( "prediction on D:" )
+    print( predict( input_values ) )
+                                
+    
     
     
